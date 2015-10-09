f90735d96d039a8525d288461211e9c4f56a5b02
Noah Camp <campn@student.swosu.edu>
2015-10-09 10:40:29 -0500

    Added outline

--- src/additiongame.java
+++ src/additiongame.java
@@ -3,6 +3,23 @@ public class additiongame {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Fellow Programmer!");
+		//Outline
+		/*
+		 * Round 1
+		 * Generate two random numbers
+		 * Ask the user to add these two numbers
+		 * if correct
+		 * 	tell them they are correct
+		 * 	award them points
+		 * 	move them on to Round 2
+		 * if incorrect
+		 * 	tell them they were incorrect
+		 * 	give them easier question
+		 * Round 2
+		 * Round 3
+		 * Round 4
+		 * They win, give final score
+		 */
 	}
 
 }
